46a7e83e3904d1ea004fc5534dc0037dd39b3418
aaronw94 <aaronw94@ymail.com>
2018-06-25 14:33:00 -0500

    Update tableInsertions.sql

--- tableInsertions.sql
+++ tableInsertions.sql
@@ -1,6 +1,33 @@
 INSERT INTO employee
 values(12345678, 'Victor', 'Liam', NULL, 'Manager', 35);
 
+INSERT INTO employee
+values(87654321, 'Valerie', 'Edwards', NULL, 'Manager', 31);
+
+INSERT INTO employee
+values(98345791, 'Nicole', 'Marson', 87654321, 'Accountant', 24);
+
+INSERT INTO employee
+values(52347587, 'Chris', 'Coleman', 12345678, 'Attendant', 28);
+
+INSERT INTO employee
+values(24343589, 'Hanes', 'Wilson', 87654321, 'Receptionist', 26);
+
+INSERT INTO employee
+values(54839215, 'Blake', 'Nuke', NULL, 'Manager', 42);
+
+INSERT INTO employee
+values(84573902, 'Claire', 'Keller', 54839215, 'Support', 25);
+
+INSERT INTO employee
+values(38754377, 'Wayne', 'Keller', 54839215, 'Support', 21);
+
+INSERT INTO employee
+values(44693864, 'Josh', 'Eckart', 12345678, 'Attendant', 29);
+
+INSERT INTO employee
+values(62452379, 'Richard', 'Moore', 87654321, 'Accountant', 36);
+
 
 
 insert into insurance values(848204421, 'state farm', 'full coverage', 12.50, 2); 
