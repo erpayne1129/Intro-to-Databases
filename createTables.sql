60d5002e78a78ae8a6a08e1fdf6819318392b743
aaronw94 <aaronw94@ymail.com>
2018-06-25 13:55:03 -0500

    Added insurance table

--- createTables.sql
+++ createTables.sql
@@ -8,3 +8,12 @@ create table employee (
     primary key(ssn),
     foreign key (managerssn) references employee(ssn)
 )
+
+create table insurance (
+    insuranceid int,
+    owner varchar(25),
+    description varchar(25),
+    price float,
+    duration int,
+    primary key(insuranceid)
+)
