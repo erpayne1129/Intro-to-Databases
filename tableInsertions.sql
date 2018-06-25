45b5ebc3ff2aca8e451f263e284fc029d071b741
BjHorseman <38536257+BjHorseman@users.noreply.github.com>
2018-06-25 14:53:58 -0500

    Update tableInsertions.sql

--- tableInsertions.sql
+++ tableInsertions.sql
@@ -41,16 +41,16 @@ insert into insurance values(424815312, 'liberty mutual', 'liability', 10.50, 1)
 insert into insurance values(548392042, 'state farm', 'full coverage', 13.50, 6); 
 insert into insurance values(984827532, 'liberty mutual', 'full coverage', 11.50, 9); 
 
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (13235869,'Jim','Jimson','debit','1111 2222 3333 4444','123456789','jj@email.com','123-456-789',848204421);
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (25789451,'Peter','Peters','debit','2222 2222 3333 4444','123456788','pp@email.com','123-456-788',438291248);
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (33697454,'Tom','Wong','debit','3333 2222 3333 4444','123456787','qq@email.com','123-456-787',112313413);
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (48534976,'Tim','Choi','credit','4444 2222 3333 4444','123456786','ss@email.com','123-456-786',738194243);
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (52469464,'Eric','Zhang','debit','1122 2222 3333 4444','123456785','aa@email.com','123-456-785',573910313);
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (66587956,'Adu','Pham','debit','1133 2222 3333 4444','123456784','ll@email.com','123-456-784',974914891);
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (77895988,'John','White','credit','1144 2222 3333 4444','123456783','lop@email.com','123-456-783',231421232);
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (88975648,'Sam','Flynn','debit','1155 2222 3333 4444','123456782','oing@email.com','123-456-782',424815312);
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (93687549,'Lily','Huang','credit','1166 2222 3333 4444','123456781','zkzk@email.com','123-456-781',548392042);
-insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (10147526,'Zed','Allen','debit','1177 2222 3333 4444','123456780','opop@email.com','123-456-780',984827532);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (13235869,'Jim','Jimson','debit','1111 2222 3333 4444',123456789,'jj@email.com','123-456-789',848204421);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (25789451,'Peter','Peters','debit','2222 2222 3333 4444',123456788,'pp@email.com','123-456-788',438291248);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (33697454,'Tom','Wong','debit','3333 2222 3333 4444',123456787,'qq@email.com','123-456-787',112313413);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (48534976,'Tim','Choi','credit','4444 2222 3333 4444',123456786,'ss@email.com','123-456-786',738194243);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (52469464,'Eric','Zhang','debit','1122 2222 3333 4444',123456785,'aa@email.com','123-456-785',573910313);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (66587956,'Adu','Pham','debit','1133 2222 3333 4444',123456784,'ll@email.com','123-456-784',974914891);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (77895988,'John','White','credit','1144 2222 3333 4444',123456783,'lop@email.com','123-456-783',231421232);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (88975648,'Sam','Flynn','debit','1155 2222 3333 4444',123456782,'oing@email.com','123-456-782',424815312);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (93687549,'Lily','Huang','credit','1166 2222 3333 4444',123456781,'zkzk@email.com','123-456-781',548392042);
+insert into lessee( ssn, fname, lanme, paymenttype, cardnumber, licensenumber, email, phone, insurance) values (10147526,'Zed','Allen','debit','1177 2222 3333 4444',123456780,'opop@email.com','123-456-780',984827532);
 
 INSERT INTO  Vehicle (model, description, place, insurance, passengers, vin) VALUES ('2014 Civic', 'White', 'Richardson, TX', 848204421, 2, '12345');
 INSERT INTO  Vehicle (model, description, place, insurance, passengers, vin) VALUES ('2016 Accent', 'White', 'Irving, TX', 438291248, 4, '58693');
