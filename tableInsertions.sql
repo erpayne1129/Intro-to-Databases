952083fc20ff31ed0a7f0c230a63da0ebd2a329f
adetoye35 <40577380+adetoye35@users.noreply.github.com>
2018-06-25 14:32:10 -0500

    Update tableInsertions.sql

--- tableInsertions.sql
+++ tableInsertions.sql
@@ -1,2 +1,15 @@
 INSERT INTO employee
 values(12345678, 'Victor', 'Liam', NULL, 'Manager', 35);
+
+
+
+insert into insurance values(848204421, 'state farm', 'full coverage', 12.50, 2); 
+insert into insurance values(438291248, 'all state', 'liability', 14.50, 3); 
+insert into insurance values(112313413, 'geico', 'full coverage', 20, 1); 
+insert into insurance values(738194243, 'state farm', 'full coverage', 19.50, 4); 
+insert into insurance values(573910313, 'geico', 'liability', 12.50, 3); 
+insert into insurance values(974914891, 'all state', 'full coverage', 12, 6); 
+insert into insurance values(231421232, 'general', 'liability', 11, 2); 
+insert into insurance values(424815312, 'liberty mutual', 'liability', 10.50, 1); 
+insert into insurance values(548392042, 'state farm', 'full coverage', 13.50, 6); 
+insert into insurance values(984827532, 'liberty mutual', 'full coverage', 11.50, 9); 
