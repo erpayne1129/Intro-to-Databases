bc1cb2d53dd27764b490ebd0fd6b1aa7b2a4ff95
adetoye35 <40577380+adetoye35@users.noreply.github.com>
2018-06-25 14:01:22 -0500

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -31,3 +31,21 @@ create table lessee (
     primary key(ssn),
     foreign key (insurance) references insurance(insuranceid)
 )
+
+create table Vehicle(
+ type varchar(25),
+ place varchar(25),
+ insurance varchar(25),
+ size varchar(25),
+ vin varchar(25),
+ primary key(vin),
+ foreign key (vin) references insurance (id));
+ 
+ create table office (
+ city varchar(15),
+ state varchar(15),
+ postalCode int,
+ branchNumber int,
+ Mgr_SSN int,
+ Primary key (branchNumber),
+ Foreign key (Mgr_SSN) references Employee(Emp_SSN));
